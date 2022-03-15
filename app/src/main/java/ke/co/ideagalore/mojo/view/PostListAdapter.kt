9dba33372914b86eb94a6a9192ace799f00966d0
w.kariukijames <kwjames9326@gmail.com>
2022-03-15 08:57:30 +0300

    Created Post List ViewModel Class

--- app/src/main/java/ke/co/ideagalore/mojo/view/PostListAdapter.kt
+++ app/src/main/java/ke/co/ideagalore/mojo/view/PostListAdapter.kt
@@ -3,6 +3,7 @@ package ke.co.ideagalore.mojo.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ke.co.ideagalore.mojo.R
 import ke.co.ideagalore.mojo.model.Post
@@ -11,6 +12,12 @@ class PostListAdapter(private val postList:ArrayList<Post>):
     RecyclerView.Adapter<PostViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
 
+        fun updatePostList(newUpdatedPosts:List<Post>){
+            postList.clear()
+            postList.addAll(newUpdatedPosts)
+            notifyDataSetChanged()
+        }
+
         val view=LayoutInflater.from(parent.context).inflate(R.layout.post_child,parent, false)
         return  PostViewHolder(view)
 
@@ -18,6 +25,9 @@ class PostListAdapter(private val postList:ArrayList<Post>):
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
 
+        holder.id.text=postList[position].id.toString()
+        holder.title.text=postList[position].title
+        holder.body.text=postList[position].body
     }
 
     override fun getItemCount(): Int {
@@ -26,5 +36,7 @@ class PostListAdapter(private val postList:ArrayList<Post>):
 }
 
 class PostViewHolder(val view:View):RecyclerView.ViewHolder(view){
-
+    val id: TextView=view.findViewById(R.id.tv_one);
+    val title: TextView=view.findViewById(R.id.tv_two);
+    val body: TextView=view.findViewById(R.id.tv_three);
 }
---
+++ app/src/main/java/ke/co/ideagalore/mojo/viewmodel/PostListViewModel.kt
@@ -0,0 +1,4 @@
+package ke.co.ideagalore.mojo.viewmodel
+
+class PostListViewModel {
+}
